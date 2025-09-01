9bba1b1cfaf317e559cba5bac06b22128e05ccf4
Tessa1217 <96612755+Tessa1217@users.noreply.github.com>
2025-09-01 09:12:30 +0900

    Time: 38 ms (70.97%), Space: 52.6 MB (88.99%) - LeetHub

--- 2809-create-hello-world-function/2809-create-hello-world-function.js
+++ 2809-create-hello-world-function/2809-create-hello-world-function.js
@@ -2,9 +2,9 @@
  * @return {Function}
  */
 var createHelloWorld = function() {
-    
+    const returnString = "Hello World"
     return function(...args) {
-        return "Hello World"
+        return returnString
     }
 };
 
