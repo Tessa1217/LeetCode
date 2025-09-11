d2ddb8361e50c3699b937c4fd52c5952ddfa899f
Tessa1217 <96612755+Tessa1217@users.noreply.github.com>
2025-09-11 14:06:32 +0900

    Time: 13 ms (5.65%), Space: 63.2 MB (5.17%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function(n) {
+    if (n === 1) return '1'
+    return helper(countAndSay(n - 1))
+};
+
+const helper = (digit_str) => {
+    const rle_str_arr = []
+    const freqCnt = () => {
+        const result = []
+        result.push([digit_str[0], 1])
+        for (let i = 1; i < digit_str.length; i++) {
+            if (digit_str[i] !== digit_str[i - 1]) {
+                result.push([digit_str[i], 0])
+            }
+            result[result.length - 1][1]++
+        }
+        return result
+    }
+    // frequency counting
+    const frequency_array = freqCnt()
+    for (const [str, freq] of frequency_array) {
+        rle_str_arr.push(freq.toString().concat(str))
+    }
+    return rle_str_arr.join("")
+}
+
+function freqHelper(digit_str) {
+    console.log(digit_str)
+    const result = []
+    result.push([digit_str[0], 1])
+    for (let i = 1; i < digit_str.length; i++) {
+        if (digit_str[i] !== digit_str[i - 1]) {
+            result.push([digit_str[i], 0])
+        }
+        result[result.length - 1][1]++
+    }
+    return result
+}
+
+function stringHelper (array) {
+    return array.map(([str, freq]) => String(freq).concat(str)).join("")
+} 
