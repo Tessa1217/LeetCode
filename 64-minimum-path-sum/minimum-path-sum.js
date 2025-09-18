b2ae080a08c335bfc2ebb31487b543b429bb48c8
Tessa1217 <96612755+Tessa1217@users.noreply.github.com>
2025-09-18 23:31:02 +0900

    Time: 0 ms (100.00%) | Memory: 56 MB (79.09%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.js
+++ 64-minimum-path-sum/minimum-path-sum.js
@@ -5,8 +5,7 @@
 var minPathSum = function(grid) {
     const m = grid.length;
     const n = grid[0].length;
-    const visited = Array.from({ length : m}, () => Array.from({length : n}).fill(false))
-
+    
     for (let i = 1; i < m; i++) {
         grid[i][0] = grid[i][0] + grid[i - 1][0]
     }
