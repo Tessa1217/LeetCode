7397837d620eaaa9c79b1e214c17f8804505d029
Tessa1217 <96612755+Tessa1217@users.noreply.github.com>
2025-09-18 23:31:42 +0900

    Time: 6 ms (61.54%) | Memory: 57.8 MB (39.44%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.js
+++ 64-minimum-path-sum/minimum-path-sum.js
@@ -6,10 +6,12 @@ var minPathSum = function(grid) {
     const m = grid.length;
     const n = grid[0].length;
     
+    // 좌측 첫번째 열
     for (let i = 1; i < m; i++) {
         grid[i][0] = grid[i][0] + grid[i - 1][0]
     }
 
+    // 첫줄 행
     for (let j = 1; j < n; j++) {
         grid[0][j] = grid[0][j] + grid[0][j - 1]
     }
