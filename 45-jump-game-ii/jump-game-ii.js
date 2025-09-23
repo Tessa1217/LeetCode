1c3c0f15ce0f8ee21672d3744df4b8b973fbd5ba
Tessa1217 <96612755+Tessa1217@users.noreply.github.com>
2025-09-24 00:38:41 +0900

    Time: 0 ms (100.00%) | Memory: 54.6 MB (89.43%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.js
+++ 45-jump-game-ii/jump-game-ii.js
@@ -9,10 +9,12 @@ var jump = function(nums) {
     let end = 0;
 
     for (let i = 0; i < n - 1; i++) {
+        // 현재 구간 시점으로 가장 멀리 갈 수 있는 지점 구하기
         farthest = Math.max(farthest, i + nums[i])
+        // 만약 i가 현재 점프 구간 종료 지점이랑 일치한다면
         if (i === end) {
             jumps++;
-            end = farthest;
+            end = farthest; // 다음 점프 구간의 끝으로 재갱신
         }
     }
 
