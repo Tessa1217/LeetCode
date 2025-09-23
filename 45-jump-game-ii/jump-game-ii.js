8bad7b009b92d6b9ae2dc769941ea772f003d799
Tessa1217 <96612755+Tessa1217@users.noreply.github.com>
2025-09-24 00:36:57 +0900

    Time: 1 ms (78.70%) | Memory: 57.4 MB (24.42%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(nums) {
+    const n = nums.length;
+    let jumps = 0;
+    let farthest = 0;
+    let end = 0;
+
+    for (let i = 0; i < n - 1; i++) {
+        farthest = Math.max(farthest, i + nums[i])
+        if (i === end) {
+            jumps++;
+            end = farthest;
+        }
+    }
+
+    return jumps;
+};
