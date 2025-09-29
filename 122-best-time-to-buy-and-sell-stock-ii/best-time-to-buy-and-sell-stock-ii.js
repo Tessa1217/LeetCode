326e29fbcb6acab044d553058d9fc72b98726975
Tessa1217 <96612755+Tessa1217@users.noreply.github.com>
2025-09-30 00:32:39 +0900

    Time: 1 ms (73.22%) | Memory: 55.1 MB (30.40%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -3,24 +3,16 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-
-    const days = prices.length
-    const dp = Array.from({length : days}, () => Array(2).fill(0))
-
-    // 첫번째 주식
-    dp[0][0] = 0 // 주식 사지 않음
-    dp[0][1] -= prices[0] // 주식 삼
     
-    for (let i = 1; i < days; i++) {
-        // 주식을 안 들고 있는 날
-            // i - 1번째 주식도 구매하지 않고, i번째 주식도 구매X
-            // i - 1번째 주식 구매분을 i날에 판 경우        
-        dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]); 
-        // 주식 들고있는 날
-            // i - 1번째 주식을 계속 들고 있음
-            // i - 1번째 주식을 구매하지 않은 상태에서 i날에 i번째 주식 구매
-        dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+    let maxProfit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        // 전날 보다 증가했다면
+        // 증가분 만큼 max profit에 더하기
+        const prev = prices[i - 1]
+        const cur = prices[i]
+        maxProfit += cur > prev ? cur - prev : 0
     }
 
-    return Math.max(dp[days - 1][0], dp[days - 1][1]);    
+    return maxProfit;
 };
